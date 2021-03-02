a3e77dc240a720ae08582dd79631dd8225c3c3a8
AbderrafiiGoutaib <63517761+AbderrafiiGoutaib@users.noreply.github.com>
2021-03-02 15:10:34 +0100

    my first commit

---
+++ src/file.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class file extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Hello this is me </h4>
+        <h2>Abderrafii goutaib </h2>
+      </div>
+    );
+  }
+}
