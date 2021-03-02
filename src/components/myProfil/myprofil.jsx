a99e17dfdb93d0db633a0a6d551e2bad128a38b4
AbderrafiiGoutaib <63517761+AbderrafiiGoutaib@users.noreply.github.com>
2021-03-02 15:47:04 +0100

    some edit

---
+++ src/components/myProfil/myprofil.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export class myprofil extends Component {
+  render() {
+    return (
+      <div>
+        <h1>My Profil</h1>
+        <h4>name : Abderrafii</h4>
+        <h4>frontend developer intern at xhub</h4>
+        <h4>
+          I am passionate by the remarkable effect technology has on our lives.{' '}
+        </h4>
+      </div>
+    );
+  }
+}
+
+export default myprofil;
